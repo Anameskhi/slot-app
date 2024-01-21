5c3e8f5665244e40ab7a4bf3b07c21841034ee2a
Anameskhi <a.meskhii@gmail.com>
2024-01-21 23:19:01 +0400

    added new styles

--- src/app/pages/slots/slots.component.ts
+++ src/app/pages/slots/slots.component.ts
@@ -1,31 +1,57 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlotService } from 'src/app/core/services/slot.service';
+import { SlotCategoryNavBarSVG } from './models/slot-svg';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-slots',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './slots.component.html',
-  styleUrls: ['./slots.component.scss']
+  styleUrls: ['./slots.component.scss'],
 })
 export class SlotsComponent {
-  constructor(private slotService: SlotService) {}
+  constructor(
+    private slotService: SlotService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   games!: any[];
   visibleCategories: any[] = [];
   hiddenCategories: any[] = [];
-  initialDisplayCount = 8;
+  initialDisplayCount = 10;
   isExpanded = false;
 
+  categoryNavBarInfo = SlotCategoryNavBarSVG;
+  cardsInfo = [
+    'Burning Hot',
+    'Burning Hot',
+    'Burning Hot',
+    'Burning Hot',
+    'Starburst',
+    'Burning Hot',
+    'Burning Hot',
+    'Starburst',
+    'Burning Hot',
+    'Burning Hot',
+    'Starburst',
+  ];
+
   ngOnInit(): void {
-    this.slotService.getAllCategories().subscribe(res => {
+    this.slotService.getAllCategories().subscribe((res) => {
       this.games = res.data;
       this.visibleCategories = this.games.slice(0, this.initialDisplayCount);
       this.hiddenCategories = this.games.slice(this.initialDisplayCount);
       console.log(this.hiddenCategories);
     });
-    this.slotService.getProvidersList().subscribe(res=>console.log(res.data))
+    this.slotService
+      .getProvidersList()
+      .subscribe((res) => console.log(res.data));
+  }
+
+  getSanitizedSvg(svgString: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(svgString);
   }
 
   toggleCategories(): void {
